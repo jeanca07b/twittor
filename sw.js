2884adc64acb2b46e1cdd466f93946dfa1864f99
Jean Carlos Arias Baltodano <jcarlosariasb@hotmail.com>
2021-09-27 18:58:41 -0600

    Modificacion SW3

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
+// imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1';
+const STATIC_CACHE = 'static-v3';
+const DYNAMIC_CACHE = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v2';
 
 //todo lo que hacemos nosotros
 const APP_SHELL = [
-    //'/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
