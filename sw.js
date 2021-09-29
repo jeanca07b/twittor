da79cd9d1e18a04cc35715a1baca464396b872a9
Jean Carlos Arias Baltodano <jcarlosariasb@hotmail.com>
2021-09-29 17:54:49 -0600

    Modificacion para dynamic audit 2

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v5';
-const DYNAMIC_CACHE = 'dynamic-v3';
+const STATIC_CACHE = 'static-v6';
+const DYNAMIC_CACHE = 'dynamic-v4';
 const INMUTABLE_CACHE = 'inmutable-v2';
 
 //todo lo que hacemos nosotros
